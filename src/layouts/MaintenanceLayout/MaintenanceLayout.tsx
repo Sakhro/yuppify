bebd1af0d7774f08fde7a73cef46b068bd1828b0
Sava-Danylo Sakhro <sd.sakhro@gmail.com>
2022-04-10 22:40:51 +0300

    Fix maintenance mode

--- src/layouts/MaintenanceLayout/MaintenanceLayout.tsx
+++ src/layouts/MaintenanceLayout/MaintenanceLayout.tsx
@@ -2,13 +2,16 @@ import React from "react";
 
 import Typography from "@mui/material/Typography";
 
+export const MAINTENANCE_MODE: boolean =
+  !!process.env.NEXT_PUBLIC_MAINTENANCE_MODE && JSON.parse(process.env.NEXT_PUBLIC_MAINTENANCE_MODE as string);
+
 export const MaintenanceLayout: React.FC = (props) => (
   <>
-    {!!process.env.NEXT_PUBLIC_MAINTENANCE_MODE && (
+    {MAINTENANCE_MODE && (
       <Typography variant="h4" pt={10} align="center">
         Website is in Maintenance mode. We'll be right back!
       </Typography>
     )}
-    {!process.env.NEXT_PUBLIC_MAINTENANCE_MODE && props.children}
+    {!MAINTENANCE_MODE && props.children}
   </>
 );
